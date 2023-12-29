478c68dcb7f2b9e0756ca09cee219cd11de06c19
Mehreen-whjr <101886787+Mehreen-whjr@users.noreply.github.com>
2023-12-28 20:56:34 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,56 @@
-function preload() {
-    musicFile1 = loadSound('music.mp3');
-    musicFil21 = loadSound('music2.mp3');
-  }
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    center();
-    webcam = createCapture(VIDEO);
-    webcam.hide();
-  }
-function draw() {
-    background(0);
-    image(webcam, 0, 0, width, height);
-  }
+song = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload() 
+{
+    song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup() 
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() 
+{
+    console.log('PoseNet is Initialized');
+}
+
+function draw() 
+{
+    image(video, 0, 0, 600, 500);
+}
+
+function play() 
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses()
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
+}
